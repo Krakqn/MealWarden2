4971eb169e58c2229eb9e4f7d3e225a872f82dc0
Krakqn <139305417+Krakqn@users.noreply.github.com>
2024-04-01 00:27:12 -0700

    Added User-Agent HTTP header.

--- GeminiMultiturnChat/BarcodeScanner/OpenFoodFacts/OpenFoodFactsAPI.swift
+++ GeminiMultiturnChat/BarcodeScanner/OpenFoodFacts/OpenFoodFactsAPI.swift
@@ -18,23 +18,27 @@ struct Product: Codable {
 class OpenFoodFactsAPI {
     private var errorMessage = "Error: Unable to Find Ingredients"
     
-    func fetchData(barcode: String, completion: @escaping (String) -> Void) {
-        guard let url = URL(string: "https://world.open foodfacts.org/api/v2/product/\(barcode)")
-        else { completion(self.errorMessage); return }
-
-        let session = URLSession.shared
-        let task = session.dataTask(with: url) {
-            data, response, error in
-            do {
-                guard let data = data
-                else { throw NSError(domain: "NoDataError", code: 0, userInfo: nil) }
-                
-                let jsonObject = try JSONDecoder().decode(ProductInformation.self, from: data)
-                DispatchQueue.main.async { completion(jsonObject.product.ingredients_text); return }
-            }
-            catch { completion(self.errorMessage); return }
-        }
-        
-        task.resume()
-    }
+  func fetchData(barcode: String, completion: @escaping (String) -> Void) {
+      guard let url = URL(string: "https://world.openfoodfacts.org/api/v2/product/\(barcode)")
+      else { completion(self.errorMessage); return }
+      
+      var request = URLRequest(url: url)
+      request.setValue("MealWarden - iOS - Version 1.0", forHTTPHeaderField: "User-Agent")
+      
+      let session = URLSession.shared
+      let task = session.dataTask(with: request) {
+          data, response, error in
+          
+          do {
+              guard let data = data
+              else { throw NSError(domain: "NoDataError", code: 0, userInfo: nil) }
+              
+              let jsonObject = try JSONDecoder().decode(ProductInformation.self, from: data)
+              DispatchQueue.main.async { completion(jsonObject.product.ingredients_text); return }
+          }
+          catch { completion(self.errorMessage); return }
+      }
+      
+      task.resume()
+  }
 }
