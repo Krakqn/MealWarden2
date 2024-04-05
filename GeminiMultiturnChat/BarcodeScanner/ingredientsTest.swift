fc1205bd8656e8ec79207bb7c95f040d8b89be18
Akksharvan Senthilkumar <108592251+Akksharvan@users.noreply.github.com>
2024-04-04 21:54:17 -0400

    Updated API for more elements

--- GeminiMultiturnChat/BarcodeScanner/ingredientsTest.swift
+++ GeminiMultiturnChat/BarcodeScanner/ingredientsTest.swift
@@ -8,20 +8,24 @@
 import SwiftUI
 
 struct ingredientsTest: View {
-    @State private var product: Product?
+    @State private var productInformation: ProductInformation?
     
-    var barcode: String = "3017620422003"
+    var barcode: String = "9948247970"
     
     var body: some View {
         VStack {
-            if let product = product {
-                Text("Product Name: \(product.product_name)")
-                Text("Nutri-Score Grade: \(product.nutriscore_grade)")
-                Text("Allergens: \(product.allergens ?? "")")
-                Text("Traces: \(product.traces ?? "")")
-                Text("Labels: \(product.labels ?? "")")
-              Text("Ingredients: \(product.ingredients_text)")
-                
+            if let productInformation = productInformation {
+                if (productInformation.status_verbose == "product not found") {
+                    Text("Product Not Available")
+                }
+                else {
+                    Text("Product Name: \(productInformation.product?.product_name ?? "")")
+                    Text("Nutri-Score Grade: \(productInformation.product?.nutriscore_grade ?? "")")
+                    Text("Allergens: \(productInformation.product?.allergens ?? "")")
+                    Text("Traces: \(productInformation.product?.traces ?? "")")
+                    Text("Labels: \(productInformation.product?.labels ?? "")")
+                    Text("Ingredients: \(productInformation.product?.ingredients_text ?? "")")
+                }
             } else {
                 Text("Loading...")
             }
@@ -30,7 +34,7 @@ struct ingredientsTest: View {
         .onAppear {
             let API = OpenFoodFactsAPI()
             API.fetchData(barcode: barcode) { result in
-                self.product = result
+                self.productInformation = result
             }
         }
     }
